1edc97abee8840158ee8aaaf2a525e9b07be93c2
Daria Utkina <daria_utkina_ej@gmail.com>
2023-02-27 15:36:16 +0300

    add error page

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -17,11 +17,15 @@ class CategoryController extends AppController{
 
     public function actionView($id){
         $id = Yii::$app->request->get('id');
+
+        $category = Category::findOne($id);
+        if(empty($category))
+            throw new \yii\web\HttpException(404, 'Такой категории нет');
+
 //        $products = Product::find()->where(['category_id' => $id])->all();
         $query = Product::find()->where(['category_id' => $id]);
         $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 3, 'forcePageParam' => false, 'pageSizeParam' => false]);
         $products = $query->offset($pages->offset)->limit($pages->limit)->all();
-        $category = Category::findOne($id);
         $this->setMeta('E-SHOPPER | ' . $category->name, $category->keywords, $category->description);
         return $this->render('view', compact('products', 'pages', 'category'));
     }
