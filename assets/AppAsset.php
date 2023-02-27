016003adf680959139d78aa64cf9073064464e8f
Daria Utkina <daria_utkina_ej@gmail.com>
2023-02-27 09:48:02 +0300

    first commit

---
+++ assets/AppAsset.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+//      'css/bootstrap.min.css',
+        'css/font-awesome.min.css',
+        'css/prettyPhoto.css',
+        'css/price-range.css',
+        'css/animate.css',
+        'css/main.css',
+        'css/responsive.css',
+    ];
+    public $js = [
+  //      'js/jquery.js',
+   //     'js/bootstrap.min.js',
+        'js/jquery.scrollUp.min.js',
+        'js/price-range.js',
+        'js/jquery.prettyPhoto.js',
+        'js/main.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
