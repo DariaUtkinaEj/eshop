c5a080f914ed368030b06f3c318fc828263518da
Daria Utkina <daria_utkina_ej@gmail.com>
2023-02-27 12:01:17 +0300

    add category controller

---
+++ controllers/AppController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class AppController extends Controller{
+
+}
--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace app\controllers;
+use app\models\Category;
+use app\models\Product;
+use Yii;
 
-class CategoryController
-{
+class CategoryController extends AppController{
+
+    public function actionIndex(){
+        $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
+        return $this->render('index', compact('hits'));
+    }
+
+    public function actionView($id){
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])->all();
+        return $this->render('view', compact('products'));
+    }
 
 }
