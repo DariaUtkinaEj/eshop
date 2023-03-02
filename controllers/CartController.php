462792eb3522ea374e12b314bb46088b33fc389e
Daria Utkina <daria_utkina_ej@gmail.com>
2023-03-02 13:36:19 +0300

    fix cart

---
+++ controllers/CartController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Product;
+use app\models\Cart;
+use Yii;
+
+/*Array
+(
+    [1] => Array
+    (
+        [qty] => QTY
+        [name] => NAME
+        [price] => PRICE
+        [img] => IMG
+    )
+    [10] => Array
+    (
+        [qty] => QTY
+        [name] => NAME
+        [price] => PRICE
+        [img] => IMG
+    )
+)
+    [qty] => QTY,
+    [sum] => SUM
+);*/
+
+class CartController extends AppController{
+
+    public function actionAdd(){
+        $id = Yii::$app->request->get('id');
+        $product = Product::findOne($id);
+        if(empty($product)) return false;
+        $session =Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->addToCart($product);
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+    public function actionClear(){
+        $session =Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.qty');
+        $session->remove('cart.sum');
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+}
---
+++ views/cart/cart-modal.php
@@ -0,0 +1,36 @@
+<?php if(!empty($session['cart'])): ?>
+    <div class="table-responsive">
+        <table class="table table-hover table-striped">
+            <thead>
+            <tr>
+                <th>Фото</th>
+                <th>Наименование</th>
+                <th>Кол-во</th>
+                <th>Цена</th>
+                <th><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($session['cart'] as $id => $item):?>
+                <tr>
+                    <td><?= \yii\helpers\Html::img("@web/images/products/{$item['img']}", ['alt' => $item['name'], 'height' => 50]) ?></td>
+                    <td><?= $item['name']?></td>
+                    <td><?= $item['qty']?></td>
+                    <td><?= $item['price']?></td>
+                    <td><span data-id="<?= $id?>" class="glyphicon glyphicon-remove text-danger del-item" aria-hidden="true"></span></td>
+                </tr>
+            <?php endforeach?>
+            <tr>
+                <td colspan="4">Итого: </td>
+                <td><?= $session['cart.qty']?></td>
+            </tr>
+            <tr>
+                <td colspan="4">На сумму: </td>
+                <td><?= $session['cart.sum']?></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+<?php else: ?>
+    <h3>Корзина пуста</h3>
+<?php endif;?>
